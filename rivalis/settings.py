9c8d194abb8b2ce17a41ff597a5b9247f5c80f3c
zetsumeishi <olivier.loustaunau@gmail.com>
2020-12-15 14:34:05 -0800

    Added local static files management

--- rivalis/settings.py
+++ rivalis/settings.py
@@ -130,3 +130,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
