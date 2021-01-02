6616242aa2222cb276449d25f0ef7dfb7ab1cbb7
zetsumeishi <olivier.loustaunau@gmail.com>
2021-01-02 11:29:53 -0800

    Fixed static folders in settings

--- rivalis/settings.py
+++ rivalis/settings.py
@@ -132,9 +132,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
