c7cf16ffa6f81e3c2bb9ea4ed827b94db053737e
zetsumeishi <olivier.loustaunau@gmail.com>
2021-01-12 09:07:25 -0800

    Now using faker through factory_boy to generate model data

--- users/tests/factories.py
+++ users/tests/factories.py
@@ -3,11 +3,9 @@ import random
 import factory.fuzzy
 from django.contrib.auth import get_user_model
 from django.utils import timezone
-from faker import Faker
 
 from users.constants import TIMEZONES
 
-fake = Faker()
 timezones = [tz[0] for tz in TIMEZONES]
 
 
@@ -16,17 +14,17 @@ class UserFactory(factory.django.DjangoModelFactory):
         model = get_user_model()
 
     id = factory.Sequence(lambda n: n)
-    first_name = fake.first_name()
-    last_name = fake.last_name()
-    password = fake.password(length=12)
     is_active = True
     date_joined = timezone.now()
     last_login = timezone.now()
-    username = factory.Sequence(lambda n: f'player{n}')
-    email = factory.Sequence(lambda n: f'player{n}@rivalis.gg')
-    riot_id = username
+    username = factory.Faker('user_name')
+    email = factory.Faker('ascii_safe_email')
+    password = factory.Faker('password', length=12)
+    riot_id = factory.LazyAttribute(lambda n: n.username)
     riot_tag = f'#{random.randint(100, 999)}'
     timezone = factory.fuzzy.FuzzyChoice(timezones)
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
 
 
 class StaffFactory(UserFactory):
