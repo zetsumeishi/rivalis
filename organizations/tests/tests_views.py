7311c6d2c6605fa8de27d76b18b75fcfb1f9f9d4
zetsumeishi <olivier.loustaunau@gmail.com>
2021-01-24 09:34:14 -0800

    Add tests for organization related views

--- organizations/tests/tests_views.py
+++ organizations/tests/tests_views.py
@@ -3,17 +3,70 @@ from django.test import TestCase
 from django.urls import reverse
 from slugify import slugify
 
-from disciplines.tests.factories import DisciplineFactory
+from organizations.models import Organization
 from organizations.models import Team
 from organizations.tests.factories import OrganizationFactory
+from organizations.tests.factories import TeamFactory
 from users.tests.factories import UserFactory
 
 
 class OrganizationsViewsTests(TestCase):
     def setUp(self):
         self.owner = UserFactory()
-        self.discipline = DisciplineFactory()
         self.organization = OrganizationFactory(owner=self.owner)
+        self.team = TeamFactory(organization=self.organization)
+
+    def tests_detail_organization(self):
+        """Tests for organizations.views.detail_organization"""
+
+        payload = {
+            'organization_slug': self.organization.slug,
+        }
+        url = reverse('organizations:detail_organization', kwargs=payload)
+        self.client.force_login(self.owner)
+
+        # [GET] A single team page
+        response = self.client.get(url, payload)
+
+        self.assertEqual(response.status_code, 200)
+
+    def tests_create_organization(self):
+        """Tests for organizations.views.create_organization"""
+
+        url = reverse('organizations:create_organization')
+        self.client.force_login(self.owner)
+        name = 'Rivalis'
+        short_name = 'RIV'
+        slug = slugify(name)
+        description = 'Description of team Rivalis.'
+        twitch = 'https://twitch.com'
+
+        # [POST] Create an organization with an empty name
+        payload = {
+            'name': '',
+            'short_name': short_name,
+            'slug': slug,
+            'description': description,
+            'twitch': twitch,
+        }
+        response = self.client.post(url, payload)
+
+        with self.assertRaises(ObjectDoesNotExist):
+            Organization.objects.get(slug=slug)
+        self.assertEqual(response.status_code, 200)
+
+        # [POST] Create a team with valid args
+        payload.update(name=name)
+        response = self.client.post(url, payload, follow=True)
+        organization = response.context['organization']
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(organization)
+        self.assertEqual(organization.name, name)
+        self.assertEqual(organization.short_name, short_name)
+        self.assertEqual(organization.slug, slug)
+        self.assertEqual(organization.twitch, twitch)
+        self.assertEqual(organization.owner, self.owner)
 
     def tests_detail_team(self):
         """Tests for organizations.views.detail_team"""
