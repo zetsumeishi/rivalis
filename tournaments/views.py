536e20f12126a85006dbcf5624236113fa52266a
zetsumeishi <olivier.loustaunau@gmail.com>
2021-02-16 20:58:40 +0100

    Add the tournament registratoin template and view

--- tournaments/views.py
+++ tournaments/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse
 
 from .controllers import BracketController
+from .forms import RegistrationForm
 from .forms import TournamentForm
 from .models import Tournament
 
@@ -42,6 +43,22 @@ def detail_tournament(request, tournament_slug):
         'results_js': results_js,
     }
     return render(request, 'tournaments/detail.html', context)
+
+
+def tournament_registration(request, tournament_slug):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST, request=request)
+        if form.is_valid():
+            form.save(tournament_slug=tournament_slug)
+            kwargs = {
+                'tournament_slug': tournament_slug,
+            }
+            return redirect(reverse('tournaments:detail_tournament', kwargs=kwargs))
+        else:
+            messages.add_message(request, messages.ERROR, 'Mistakes were made')
+    form = RegistrationForm(request=request)
+    context = {'form': form}
+    return render(request, 'tournaments/register.html', context)
 
 
 def tournaments(request):
