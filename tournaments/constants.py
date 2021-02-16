b331424f555ef38f8bb24d08ecf154e2f84059a5
zetsumeishi <olivier.loustaunau@gmail.com>
2021-02-16 20:45:10 +0100

    Add Tournaments models (Tournament, Stage, Round, Match, TournamentMembership) and constants

---
+++ tournaments/constants.py
@@ -0,0 +1,42 @@
+REGION_CHOICES = (
+    ('br', 'BR'),
+    ('eune', 'EUNE'),
+    ('euw', 'EUW'),
+    ('lan', 'LAN'),
+    ('las', 'LAS'),
+    ('na', 'NA'),
+    ('oce', 'OCE'),
+    ('tr', 'TR'),
+    ('jp', 'JP'),
+    ('kr', 'KR'),
+)
+
+
+# Tournament sizes
+# [(4, 4), (8, 8), (16, 16), ...]
+SIZE_CHOICES = [(2 ** i, 2 ** i) for i in range(2, 12, 1)]
+
+# Number of rounds depending on the number of participants
+# {4: 2, 8: 3, 16: 4, 32: 5, ...}
+SIZES_ROUNDS = {2 ** i: j for i, j in zip(range(2, 10), range(2, 10))}
+
+NB_MATCHES_PER_ROUND_LB = {2: 1, 4: 2, 8: 2, 16: 2, 32: 4, 64: 4}
+
+SINGLE_ELIMINATION = 'single elimination'
+DOUBLE_ELIMINATION = 'double elimination'
+
+STAGE_FORMAT_CHOICES = (
+    (SINGLE_ELIMINATION, SINGLE_ELIMINATION.title()),
+    (DOUBLE_ELIMINATION, DOUBLE_ELIMINATION.title()),
+)
+
+# TournamentMembership
+WAITING = 'waiting'
+ACCEPTED = 'accepted'
+REJECTED = 'rejected'
+
+STATUS_CHOICES = (
+    (WAITING, WAITING.title()),
+    (ACCEPTED, ACCEPTED.title()),
+    (REJECTED, REJECTED.title()),
+)
---
+++ tournaments/models.py
@@ -0,0 +1,124 @@
+from django.db import models
+from django.utils import timezone
+
+from .constants import ACCEPTED
+from .constants import REGION_CHOICES
+from .constants import SIZE_CHOICES
+from .constants import STAGE_FORMAT_CHOICES
+from .constants import STATUS_CHOICES
+from disciplines.models import Discipline
+from organizations.models import Organization
+from organizations.models import Team
+
+
+class Tournament(models.Model):
+    # Fields
+    description = models.TextField('Description', blank=True)
+    name = models.CharField('Name', max_length=255)
+    prizes = models.TextField('Prizes', blank=True)
+    region = models.CharField(
+        'Region',
+        max_length=5,
+        choices=REGION_CHOICES,
+        default='euw',
+    )
+    rules = models.TextField('Rules')
+    size = models.IntegerField(
+        'Size',
+        choices=SIZE_CHOICES,
+        default=4,
+    )
+    slug = models.CharField('Slug', max_length=255, unique=True)
+    start_date = models.DateTimeField(auto_now_add=True)
+
+    # Relationships
+    discipline = models.ForeignKey(Discipline, on_delete=models.CASCADE)
+    organizer = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    participants = models.ManyToManyField(Team, through='TournamentMembership')
+
+    def __str__(self):
+        return (
+            f'{self.name} by {self.organizer.name} on {self.discipline} | {self.region}'
+        )
+
+    @property
+    def registration_is_open(self):
+        return self.start_date > timezone.now()
+
+    @property
+    def is_full(self):
+        tournament_memberships = TournamentMembership.objects.filter(
+            tournament=self,
+            status=ACCEPTED,
+        )
+        return tournament_memberships == self.size
+
+    @property
+    def accepted_participants(self):
+        return Team.objects.filter(tournamentmembership__status=ACCEPTED)
+
+
+class TournamentMembership(models.Model):
+    seed = models.IntegerField(default=0)
+    status = models.CharField(
+        'Status',
+        max_length=12,
+        choices=STATUS_CHOICES,
+        default='waiting',
+    )
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.tournament.name} | {self.team.name}'
+
+
+class Stage(models.Model):
+    # Fields
+    format = models.CharField(
+        'Format',
+        max_length=32,
+        choices=STAGE_FORMAT_CHOICES,
+        default='single_elimination',
+    )
+
+    # Relationships
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.format
+
+
+class Round(models.Model):
+    # Fields
+    first_round = models.BooleanField(default=False)
+    name = models.CharField('Name', max_length=255)
+    number = models.IntegerField(default=1)
+
+    # Relationships
+    stage = models.ForeignKey(Stage, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Match(models.Model):
+    # Fields
+    away_score = models.IntegerField('Away score')
+    home_score = models.IntegerField('Home score')
+
+    # Relationships
+    away_team = models.ForeignKey(
+        Team,
+        related_name='away_team',
+        on_delete=models.CASCADE,
+    )
+    home_team = models.ForeignKey(
+        Team,
+        related_name='home_team',
+        on_delete=models.CASCADE,
+    )
+    round = models.ForeignKey(Round, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.home_team} VS {self.away_team}'
