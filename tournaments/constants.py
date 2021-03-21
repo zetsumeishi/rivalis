643ad58ae7fbbc2b34d8368723590e28824e0f3f
zetsumeishi <olivier.loustaunau@gmail.com>
2021-03-21 11:12:05 +0100

    Remove last occurences of the double elimination format

--- tournaments/constants.py
+++ tournaments/constants.py
@@ -20,15 +20,9 @@ SIZE_CHOICES = [(2 ** i, 2 ** i) for i in range(2, 12, 1)]
 # {4: 2, 8: 3, 16: 4, 32: 5, ...}
 SIZES_ROUNDS = {2 ** i: j for i, j in zip(range(2, 10), range(2, 10))}
 
-NB_MATCHES_PER_ROUND_LB = {2: 1, 4: 2, 8: 2, 16: 2, 32: 4, 64: 4, 128: 5, 256: 5}
-
 SINGLE_ELIMINATION = 'single elimination'
-DOUBLE_ELIMINATION = 'double elimination'
 
-STAGE_FORMAT_CHOICES = (
-    (SINGLE_ELIMINATION, SINGLE_ELIMINATION.title()),
-    (DOUBLE_ELIMINATION, DOUBLE_ELIMINATION.title()),
-)
+STAGE_FORMAT_CHOICES = ((SINGLE_ELIMINATION, SINGLE_ELIMINATION.title()),)
 
 # TournamentMembership
 WAITING = 'waiting'
